6ce702fbeb8dd21a50832f85b67345798fe61f46
Andrew Cash <andrew@csh.casa>
2021-06-30 01:03:23 -0500

    working on getting sass up and running

--- components/header.tsx
+++ components/header.tsx
@@ -2,10 +2,12 @@ import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../components/header.module.css'
 
+import '../styles/styles.scss'
+
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <Link href="/" passHref>
+    <header className={styles.container}>
+      <Link href="/" passHref className='logo'>
         <Image 
           alt="Home Image" 
           src="/../public/logo.svg" 
@@ -14,11 +16,11 @@ export default function Header() {
           height="200"
         />
       </Link>
-      {/* <Link href="/" passHref>
+      <Link href="/" passHref>
         <h1 className="header__title" >Perception Zero</h1>
-      </Link> */}
+      </Link>
       <a className="header__summary snipcart-checkout snipcart-summary" href="#" style={{textDecoration: "none"}}>
-        <span className="header__price snipcart-total-price"></span>
+        <span className="header__price snipcart-total-price">Hello</span>
       </a>
     </header>
   )
--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+// const withTypescript = require('@zeit/next-typescript')
+// const withSass = require('@zeit/next-sass')
+
+const path = require('path')
+
 module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+
   reactStrictMode: true,
 }
