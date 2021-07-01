439cb8eec27e4e38e6a04721363422d72c74edc8
Andrew Cash <andrew@csh.casa>
2021-07-01 02:57:52 -0500

    Idk what I did exactly but this looks really cool

--- components/product.tsx
+++ components/product.tsx
@@ -1,5 +1,5 @@
 import {withRouter} from 'next/router'
-import {RouterProps} from 'next/router'
+import RouterProps from "next/router"
 import Image from 'next/image'
 
 export interface IProduct {
@@ -16,30 +16,33 @@ interface IProductProps {
 }
 const Product = (props: IProductProps) => {
   return (
-    <div className="product">
-      <h2 className="product__title">{props.product.name}</h2>
-      <p className="product__description">{props.product.description}</p>
-      <Image 
-        src={props.product.image} 
-        alt="" 
-        className="product__image"
-        width='200'
-        height='200'
-      />
-      
-      <div className="product__price-button-container">
-        <div className="product__price">${props.product.price.toFixed(2)}</div>
-        <button 
-          className="snipcart-add-item product__button"
-          data-item-id={props.product.id}
-          data-item-name={props.product.name}
-          data-item-price={props.product.price}
-          data-item-url={props.router.pathname}
-          data-item-image={props.product.image}>
-          Add to cart
-        </button>
+    <div className="product-container">
+      <div className="product">
+        <h2 className="product__title">{props.product.name}</h2>
+        <p className="product__description">{props.product.description}</p>
+        <Image 
+          src={"/" + props.product.image} 
+          alt="" 
+          className="product__image"
+          layout='fill'
+          // objectFit='contain'
+        />
+        
+        <div className="product__price-button-container">
+          <div className="product__price">${props.product.price.toFixed(2)}</div>
+          <button 
+            className="snipcart-add-item product__button"
+            data-item-id={props.product.id}
+            data-item-name={props.product.name}
+            data-item-price={props.product.price}
+            data-item-url={props.router.pathname}
+            data-item-image={props.product.image}>
+            Add to cart
+          </button>
+        </div>
       </div>
     </div>
+    
   )
 }
 export default withRouter(Product)
