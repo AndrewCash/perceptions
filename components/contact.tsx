af14c910c8ed7069fc79106094667454a2c702fe
Andrew Cash <andrew@csh.casa>
2021-07-02 17:24:48 -0500

    Fix error caused by unexcaped character

--- components/contact.tsx
+++ components/contact.tsx
@@ -4,7 +4,7 @@ export default function Contact() {
   return (
     <div className="contact">
       <h2 className="contact__title">Any questions? <span className="colored">Contact us.</span></h2>
-      <p className="contact__paragraph">We're looking forward to hearing from you. Feel free to contact us if you have any questions!</p>
+      <p className="contact__paragraph">We&apos;re looking forward to hearing from you. Feel free to contact us if you have any questions!</p>
       <a href="mailto:andrew@csh.casa">
         <button>Contact Us</button>
       </a>
